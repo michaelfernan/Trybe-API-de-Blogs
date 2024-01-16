f79cf5455c1dc6e43c2bb9c543cef056139f6a68
Michael Fernandes <111091339+michaelfernan@users.noreply.github.com>
2024-01-16 12:47:21 -0300

    feat: req 2

---
+++ src/models/User.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    displayName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      field: 'display_name'
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: true
+    }
+  }, {
+    underscored: true,
+    tableName: 'users' 
+  });
+
+  return User;
+};
