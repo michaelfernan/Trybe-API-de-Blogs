443eab760682b11559501a24eda800b4b71cdee9
Michael Fernandes <111091339+michaelfernan@users.noreply.github.com>
2024-01-17 15:12:15 -0300

    add models Category

---
+++ src/models/Category.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  }, {
+    tableName: 'categories',
+    timestamps: false, // Se não houver colunas de timestamp como createdAt e updatedAt
+  });
+
+  return Category;
+};
