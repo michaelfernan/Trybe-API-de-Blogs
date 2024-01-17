cc11407d95a1466f09b77b1fb21a89664caaa5e2
Michael Fernandes <111091339+michaelfernan@users.noreply.github.com>
2024-01-17 15:19:16 -0300

    Add getAllCategories route and controller

--- src/Controller/CategoryController.js
+++ src/Controller/CategoryController.js
@@ -17,4 +17,14 @@ const createCategory = async (req, res) => {
   }
 };
 
-module.exports = { createCategory };
+const getAllCategories = async (req, res) => {
+  try {
+    const categories = await Category.findAll();
+  
+    return res.status(200).json(categories);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+};
+module.exports = { createCategory, getAllCategories };
--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ const loginController = require('./Controller/LoginController');
 const { createUser, getAllUsers, getUserById } = require('./Controller/UserControler');
 const authMiddleware = require('./middleware/authMiddleware');
 const { createCategory } = require('./Controller/CategoryController');
+const { getAllCategories } = require('./Controller/CategoryController');
 
 const router = express.Router();
 
@@ -12,4 +13,5 @@ router.post('/user', createUser);
 router.get('/user', authMiddleware, getAllUsers);
 router.get('/user/:id', authMiddleware, getUserById);
 router.post('/categories', authMiddleware, createCategory);
+router.get('/categories', authMiddleware, getAllCategories);
 module.exports = router;
